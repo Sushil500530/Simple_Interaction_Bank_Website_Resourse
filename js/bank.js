2cfeedb48840355aeb4249350b09a5cfaf94ad52
Sushil97 <sushil500530@gmail.com>
2023-08-15 09:16:11 +0600

    add other sum and reverse of the withdrow

--- js/bank.js
+++ js/bank.js
@@ -1,6 +1,34 @@
+//  step-1: addEventListener to the deposit button 
 document.getElementById('btn-deposit').addEventListener('click',function(){
     // console.log('click deposit')
+    // step -2 : get the deposit input field
+    // to input field use .value to the value inside the input field 
     const deposit = document.getElementById('input-deposit');
-    const result = deposit.value ;
-    console.log(result)
+    const newDepositAmountString = deposit.value ;
+    const newDepositAmount = parseFloat(newDepositAmountString)
+    // console.log(newDepositAmount)
+    // console.log(depositAmount)
+    // step -3 : get the current deposit total 
+    // for none input(element other than input,textArea ) use innerText to get the text 
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText ;
+    const previousDepositTotal = parseFloat(previousDepositTotalString)
+    // console.log(typeof previousDepositTotal)
+    // step -4 : add number total the deposit
+    const total = previousDepositTotal + newDepositAmount ;
+    // set the deposit total 
+    depositTotalElement.innerText = total ;
+    // console.log(total)
+    // step -5 : get balance total add  
+      const balanceElement = document.getElementById('balance-total') ;
+      const balanceTotalString = balanceElement.innerText;
+      const balanceTotal = parseFloat(balanceTotalString);
+
+      //step -6 : calculate current total balance
+      const currentBalanceTotal = balanceTotal + newDepositAmount ;
+      // set the balance total 
+    balanceElement.innerText = currentBalanceTotal ;
+    // step - : clear the deposit field 
+    deposit.value ='' ;
 })
+
---
+++ js/withdrow.js
@@ -0,0 +1,34 @@
+/*
+1. add event handler with the withdrow button
+2. get the withdrow amount from the withdrow input field
+2-5 . also make sure t convert the input into a number by using parseFloat()
+3. get previous withdrow total
+4.calculate total withdrow amount
+4-5. set taotal witdrow amount
+5. get the previous balance total
+6. calculate new balance
+6-5 . set the new balance total
+7. clear the input field
+*/
+// step -1 :
+document.getElementById('btn-withdrow').addEventListener('click',function(){
+    // step -2 :
+    const withdrowField = document.getElementById('input-withdrow')
+    const newWithdrowAmountString = withdrowField.value ;
+    const newWithdrowAmount = parseFloat(newWithdrowAmountString)
+    // console.log(typeof newWithdrowAmount) 
+
+    // step -3 :
+    const withdrowTotalElement= document.getElementById('withdrow-total') ;
+    const withdrowTotalString = withdrowTotalElement.innerText;
+    const previousWithdrowTotal = parseFloat(withdrowTotalString) ;
+    // console.log(previousWithdrowTotal)
+
+    // step-4 :
+     const currentWithdrowTotal = previousWithdrowTotal + newWithdrowAmount ;
+     withdrowTotalElement.innerText = currentWithdrowTotal;
+
+ 
+    //  step -7 : clear the input field 
+     withdrowField.value = '' ;
+})
